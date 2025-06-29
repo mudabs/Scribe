6818a7cb4c5e0de6d64073666ab8cd15244d058e
mudabs <mudaburamunashe@gmail.com>
2025-06-29 06:33:21 +0200

    Charts and Insights

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Scribe.Data; // Adjust to your namespace for the DbContext
+using Scribe.Models;
+using System.Linq;
+
+namespace Scribe.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var totalDevices = _context.SerialNumbers.Count();
+            var deadDevices = _context.SerialNumbers
+                .Where(x => x.Condition.Name == "Dead").Count();
+
+            var newDevices = _context.SerialNumbers
+                .Where(x => x.Condition.Name == "New").Count();
+
+            var inUseDevices = _context.SerialNumbers
+                .Where(x => x.ADUsersId != null).Count();
+
+            var viewModel = new DashboardViewModel
+            {
+                TotalDevices = totalDevices,
+                DeadDevices = deadDevices,
+                NewDevices = newDevices,
+                InUseDevices = inUseDevices
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Scribe.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalDevices { get; set; }
+        public int DeadDevices { get; set; }
+        public int NewDevices { get; set; }
+        public int InUseDevices { get; set; }
+    }
+}
