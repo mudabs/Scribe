9d387da62168edaf091d4738bb221323113a476c
mudabs <mudaburamunashe@gmail.com>
2025-07-19 17:41:31 +0200

    upload

---
+++ Models/UploadedRow.cs
@@ -0,0 +1,15 @@
+﻿namespace Scribe.Models
+{
+    public class UploadedRow
+    {
+        public string SerialNumber { get; set; }
+        public string AllocateToType { get; set; }  // "User" or "Group"
+        public string AllocateToName { get; set; }
+
+        public int? ResolvedUserId { get; set; }   // FK if AllocateToType == User
+        public int? ResolvedGroupId { get; set; }  // FK if AllocateToType == Group
+
+        public bool IsValid { get; set; } = true;
+        public string Error { get; set; }
+    }
+}
